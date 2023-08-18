52cfbf2d23892230a11e0fb11bd32324447d6970
ihsan1210 <ihsannugraha731@gmail.com>
2023-08-18 16:07:37 +0700

    define port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 
-const port = precess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
