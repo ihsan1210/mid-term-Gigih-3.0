fc86718594d965afaed31cc12f0f320e43ae8081
ihsan1210 <ihsannugraha731@gmail.com>
2023-08-18 19:41:57 +0700

    enable cors

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app = express()
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
-
-import cors from 'cors'
+var cors = require('cors')
 
 app.use(cors());
 
@@ -23,7 +22,8 @@ database.once('connected',()=>{
   console.log('Database Connected');
 })
 
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
