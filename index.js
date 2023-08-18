349bf005afa0d84923bb156b7c8b765b280e057c
ihsan1210 <ihsannugraha731@gmail.com>
2023-08-18 18:37:49 +0700

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
